c35bc12c9df5fecf2b199c3dc0566570aba382f6
Paulo Teixeira <paulo.teixeira541@gmail.com>
2023-08-13 11:29:03 -0300

    fix: height

--- src/app/contact/styles.ts
+++ src/app/contact/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  height: 60vh;
+  height: 70vh;
   display: flex;
   align-items: center;
   justify-content: space-around;
