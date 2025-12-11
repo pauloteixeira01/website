c6ce0ae76169aa8476b8ae65bc92b9a256d3a464
Paulo Teixeira <paulo.teixeira541@gmail.com>
2025-12-10 22:23:58 -0300

    chore: add what is a computer's article

--- src/components/ArticlesGrid/articles.ts
+++ src/components/ArticlesGrid/articles.ts
@@ -12,9 +12,25 @@ export const articles = [
       "linux-terminal",
     ],
     excerpt:
-      "A practical guide on how to safely format a USB flash drive using the terminal on Ubuntu, Debian, and derivatives",
+      "A practical guide on how to safely format a USB flash drive using the terminal on Ubuntu, Debian, and derivatives.",
     date: "2025-12-05",
     readingTime: "6 min",
     image: "/img/pendrive.svg",
+  },
+  {
+    title: "What is a computer?",
+    slug: "what-is-a-computer",
+    category: "Hardware",
+    tags: [
+      "computer",
+      "computer-science",
+      "computer-engineering",
+      "data-processor",
+    ],
+    excerpt:
+      "A concise explanation of the principles that define a computer — from information processing and algorithms to the logic that powers modern digital systems.",
+    date: "2025-12-10",
+    readingTime: "20 min",
+    image: "/img/computer.svg",
   },
 ];
